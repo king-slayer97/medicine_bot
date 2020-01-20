514da4962a06f8d21e557a4b1ed052a9192b3af8
Ayush Madan <ayumadan158@gmail.com>
2020-01-20 16:14:58 +0530

    make it better

--- config/api.js
+++ config/api.js
@@ -3,7 +3,7 @@ var apiai = require('apiai');
 // read the api.ai docs : https://api.ai/docs/
 
 //Enter your API Key
-var app = apiai('2007fee6c3d14ef889535eb0fc8b181b');
+var app = apiai('e1e9e0c1ec044a6084f4acf4f2e87fa9 ');
 
 // Function which returns speech from api.ai
 var getRes = function(query) {
